8b1aeba8948a253f8099f7805210c999d296fb3c
zainab.alkaleefa <zainab.alkaleefa@gmail.com>
2020-05-18 02:03:27 +0100

    Added the Delete and Complete Events

--- src/App.js
+++ src/App.js
@@ -10,38 +10,56 @@ import './App.css';
 
 function App() {
 
-  const [ activeTasks] = useState([
+  const [ activeTasks,setActiveTasks] = useState([
     { text: "Task1", completed: false,  dueDate: "2020-06-01", id:"001"},
     { text: "Task2",completed: false,  dueDate: "2020-07-01", id:"002" },
-    { text: "Task2", completed: false, dueDate: "2020-05-01", id:"003"}
+    { text: "Task3", completed: false, dueDate: "2020-05-01", id:"003"}
   ]);
 
-  const [ completedTasks] = useState([
-    { text: "Task1",completed: true,  dueDate: "2020-06-01", id:"001" },
-    { text: "Task2",completed: true,  dueDate: "2020-07-01", id:"002" },
-    {text: "Task3",completed: true,  dueDate: "2020-06-01", id:"003" },
-    { text: "Task4",completed: true,  dueDate: "2020-07-01", id:"004" },
-    { text: "Task5",completed: true,  dueDate: "2020-07-01", id:"005" },
-    { text: "Task6",completed: true,  dueDate: "2020-07-01", id:"006" },
-    { text: "Task7",completed: true,  dueDate: "2020-07-01", id:"007" },
-    { text: "Task8",completed: true,  dueDate: "2020-07-01", id:"008" },
-    { text: "Task9",completed: true,  dueDate: "2020-05-01", id:"009"}
+  const [ completedTasks, setCompletedTasks] = useState([
+    
   ]);
 
 
   const progressPrecentage= (completedTasks.length *100)/(completedTasks.length + activeTasks.length )
   
+  function deleteTask(id){
+    const updateTasks= activeTasks.filter(task => task.id !== id);
+    setActiveTasks(updateTasks);
+  }
+  
+
+  function completeTask(id){
+    const updateTasks= activeTasks.filter(task => task.id !== id);
+    setActiveTasks(updateTasks);
+    
+    const updateCompletedTasks= activeTasks.map(function task(task) {
+     if(task.id === id) {completedTasks.push(task);}
+    });
+    
+    setCompletedTasks(completedTasks);
+    
+
+  }
   return (
 
     <div className="main_div">
       <div className="headermargin">
-      <InputAndAdd num ={ completedTasks.length}  count={activeTasks.length+ completedTasks.length} progress={Math.round(progressPrecentage)} text={<big> &nbsp;Completed </big>}/>
+      <InputAndAdd 
+      num ={ completedTasks.length}  
+      count={activeTasks.length+ completedTasks.length} 
+      progress={Math.round(progressPrecentage)} 
+      text={<big> &nbsp;Completed </big>}/>
       </div>
       
       <NumberOfTasks  count={ activeTasks.length }  text=" Remained Tasks"/> 
 
       <div> 
-        {activeTasks.map( task => <IncompletedTask key={task.id} text={task.text}/>)}
+        {activeTasks.map( task => <IncompletedTask 
+        completeTask={completeTask}
+        deleteTask={deleteTask }
+        id={task.id} key={task.id} 
+        text={task.text}/>)}
       </div>
 
       <NumberOfTasks count= { completedTasks.length } text="Completed Tasks" /> 
--- src/IncompletedTask/IncompletedTask.js
+++ src/IncompletedTask/IncompletedTask.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './IncompletedTask.css';
 
 function IncompletedTask(props) {
+
+  function deleteClick(){
+    console.log(props.id);
+    props.deleteTask(props.id);
+  }
+
+  function completeClick(){
+    props.completeTask(props.id);
+    console.log(props.id);
+  }
   return (
     <div className="container" align ="center">
       <div className= "row col-10 list-group-item d-flex justify-content-between align-items-center ">
@@ -9,8 +19,8 @@ function IncompletedTask(props) {
             {props.text}
         </div>
         <div className="col-12 col-md-5" >
-            <button type="button" className="content3 btn btn-danger" > Complete</button>
-            <button type="button" className="content3 btn btn-danger " > Delete  </button> 
+            <button type="button" className="content3 btn btn-danger" onClick={completeClick}> Complete</button>
+            <button type="button" className="content3 btn btn-danger " onClick={deleteClick} > Delete  </button> 
         </div>
       </div>
     </div>
