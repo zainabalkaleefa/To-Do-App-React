1dd5fe622434d3f64ea24f62e23b9dc19f55ac6a
zainab.alkaleefa <zainab.alkaleefa@gmail.com>
2020-06-02 01:21:05 +0100

    Fix the database dump

--- todos.sql
+++ todos.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.30, for Linux (x86_64)
 --
--- Host: tech-returner-rds-instance.czejbfcwo5hq.eu-west-2.rds.amazonaws.com    Database: Tasks
+-- Host: tech-returner-rds-instance.czejbfcwo5hq.eu-west-2.rds.amazonaws.com    Database: todos
 -- ------------------------------------------------------
 -- Server version	5.7.22
 
@@ -14,3 +14,68 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Tasks`
+--
+
+DROP TABLE IF EXISTS `Tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Tasks` (
+  `taskId` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(255) NOT NULL,
+  `completed` tinyint(1) NOT NULL,
+  `dueDate` date NOT NULL,
+  `userId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`taskId`),
+  KEY `userId` (`userId`),
+  CONSTRAINT `Tasks_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `Users` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Tasks`
+--
+
+LOCK TABLES `Tasks` WRITE;
+/*!40000 ALTER TABLE `Tasks` DISABLE KEYS */;
+INSERT INTO `Tasks` VALUES (1,'Wash the car',0,'2020-06-10',1),(2,'Do the shopping',1,'2020-06-11',1),(3,'Walk the dog',1,'2020-06-05',1),(4,'Write a poem',0,'2020-06-06',1),(5,'Go the grandma',1,'2020-06-20',2),(6,'Finish the homework',1,'2020-06-09',2),(7,'Paint the mirror',1,'2020-07-01',2),(8,'Meet the friends',1,'2020-06-15',2),(9,'Prepare for the interview',0,'2020-06-15',3),(10,'Travel to London',0,'2020-07-10',3),(11,'Buy a new suit',0,'2020-06-14',3),(12,'Go the the emma\'s birthday',0,'2020-06-06',3);
+/*!40000 ALTER TABLE `Tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(255) NOT NULL,
+  `userEmail` varchar(255) NOT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'Paul Mottram','paul.mottram@hotmail.com'),(2,'Amanda Said','amandasaid@gmail.com'),(3,'Kate Jones','katy1984@gmail.com');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-02  1:19:02
